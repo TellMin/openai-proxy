4badd5ed8bb8f8c683e54490c3c6aa40ac5fa6bb
TellMin <dhgirkcls@gmail.com>
2023-08-19 23:52:06 +0900

    Call openai api

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
+import OpenAI from "openai";
 
-const app = new Hono();
+type Bindings = {
+  OPENAI_API_KEY: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
 
 app.use("*", logger());
-app.get("/", (c) => c.text("Hello Hono!"));
+
+app.get("/chat", async (c) => {
+  const openai = new OpenAI({
+    apiKey: c.env.OPENAI_API_KEY,
+  });
+
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: "Hello, how are you?",
+      },
+    ],
+    model: "gpt-3.5-turbo",
+  });
+
+  return c.json(completion.choices);
+});
 
 export default app;
