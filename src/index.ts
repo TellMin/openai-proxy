163dc8f85dcbd5ed8fe98b4c332aafdf38cfbfa4
TellMin <dhgirkcls@gmail.com>
2023-08-29 23:10:02 +0900

    Modify: JWT verification to use hono/jwt

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import { Hono } from "hono";
+import { Context, Hono, Next } from "hono";
 import { logger } from "hono/logger";
-import { chat } from "./openai";
 import { cors } from "hono/cors";
+import { jwt } from "hono/jwt";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import OpenAI from "openai";
 
 type Bindings = {
   OPENAI_API_KEY: string;
@@ -14,13 +16,44 @@ app.use("*", logger());
 
 app.use("*", cors());
 
-app.post("/", async (c) =>
-  chat(
-    c.req,
-    c.env.OPENAI_API_KEY,
-    c.req.header("Authorization") ?? "",
-    c.env.SECRET
-  )
+app.post(
+  "/",
+
+  async (
+    c: Context<{
+      Bindings: Bindings;
+    }>,
+    next: Next
+  ) => {
+    return jwt({ secret: c.env.SECRET })(c, next);
+  },
+
+  async (c) => {
+    const openai = new OpenAI({ apiKey: c.env.OPENAI_API_KEY });
+    const { messages } = await c.req.json();
+
+    // Ask OpenAI for a streaming chat completion given the prompt
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      stream: true,
+      messages,
+    });
+
+    // Convert the response into a friendly text-stream
+    const stream = OpenAIStream(response);
+    // Respond with the stream
+    return new StreamingTextResponse(stream);
+  }
 );
+
+app.options("*", (c) => {
+  return new Response(null, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+});
 
 export default app;
