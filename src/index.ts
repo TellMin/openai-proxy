1c5cfe246a8584e0f3efa43761b5fa24e7a2018d
TellMin <dhgirkcls@gmail.com>
2023-10-04 22:46:46 +0900

    Function_calling sample

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { logger } from "hono/logger";
 import { cors } from "hono/cors";
 import { jwt } from "hono/jwt";
 import OpenAI from "openai";
+import { prettyJSON } from "hono/pretty-json";
 
 type Bindings = {
   OPENAI_API_KEY: string;
@@ -41,6 +42,45 @@ app.post("/", async (c) => {
       await stream.write(message.choices[0]?.delta.content ?? "");
     }
   });
+});
+
+app.get("/function", async (c) => {
+  const openai = new OpenAI({ apiKey: c.env.OPENAI_API_KEY });
+
+  const messages: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [
+    {
+      content: "I am a highly intelligent question answering bot.",
+      role: "assistant",
+    },
+    {
+      content: "Tell me tomorrow's weather. Today is 2021-10-13.",
+      role: "user",
+    },
+  ];
+
+  const functions: OpenAI.Chat.Completions.ChatCompletionCreateParams.Function[] =
+    [
+      {
+        name: "tomorrows_weather",
+        description: "Get tomorrow's weather",
+        parameters: {
+          type: "object",
+          properties: {
+            date: { type: "string" },
+          },
+        },
+      },
+    ];
+
+  // Ask OpenAI for a streaming chat completion given the prompt
+  const completion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages,
+    functions: functions,
+  });
+
+  const message = completion.choices[0]?.message;
+  return c.json(message);
 });
 
 app.options("*", (c) => {
